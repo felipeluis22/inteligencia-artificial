ecf7eab9444ffb0721606426e72e19b0c4ad6272
luis felipe <114739100+felipeluis22@users.noreply.github.com>
2024-06-06 08:18:15 -0300

    a

--- script.js
+++ script.js
@@ -6,15 +6,11 @@ const textoResultado = document.querySelector (",texto-resultado");
 
 const perguntas = [
     {
-        enunciado "pergunta1",
-        alternativas ["alternativa1", "alternativa2"]
-
+        enunciado: "pergunta1",
+        alternativas: ["alternativa1", "alternativa2"]
     },
-
     {
-
-        enunciado "pergunta2",
-        alternativas ["alternativa1", "alternativa2"]
-
+        enunciado: "pergunta2",
+        alternativas: ["alternativa1", "alternativa2"]
     }
 ];
