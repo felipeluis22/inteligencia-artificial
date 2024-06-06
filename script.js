08ce6d694dc5e1697fc756a3f1c828e8eb2bfaab
luis felipe <114739100+felipeluis22@users.noreply.github.com>
2024-06-06 08:16:01 -0300

    f

---
+++ script.js
@@ -0,0 +1,20 @@
+const caixaPrincipal = document.querySelector (",caixa-principal");
+const caixaPergunta = document.querySelector (",caixa-pergunta");
+const caixaAlternativa = document.querySelector (",caixa-alternativa");
+const caixaResultado = document.querySelector (",caixa-resultado");
+const textoResultado = document.querySelector (",texto-resultado");
+
+const perguntas = [
+    {
+        enunciado "pergunta1",
+        alternativas ["alternativa1", "alternativa2"]
+
+    },
+
+    {
+
+        enunciado "pergunta2",
+        alternativas ["alternativa1", "alternativa2"]
+
+    }
+];
